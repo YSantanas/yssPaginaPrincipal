32b6aa07a01ff946c896121b00c1d72c182de7c8
YSantanas <44857937+YSantanas@users.noreply.github.com>
2025-03-20 13:37:16 -0600

    feat: agregar servicios
    se añade seccion de servicios

--- includes/header.php
+++ includes/header.php
@@ -25,6 +25,7 @@ $contenidoEquipo = [
     <link rel="stylesheet" href="./assets/css/styles.css" />
     <link rel="stylesheet" href="./assets/css/bootstrap.min.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
     <script src="./assets/js/titulos.js" defer></script>
 
---
+++ servicios.php
@@ -0,0 +1,217 @@
+<?php include("./includes/header.php"); ?>
+
+
+<main>
+
+
+    <div class="container-fluid p-2 mb-2 my-5">
+        <div class="row no-gutters">
+
+            <!-- Primera columna -->
+            <div class="col-12  d-flex flex-column justify-content-center align-items-center"
+                style=" height: auto;">
+
+                <h1 class="tituloContenedorL">Nuestra especialidad las 3 <strong> "S"</strong>:</h1>
+                <ul style="list-style: none; padding-top: 2%; color: black;">
+                    <li style="display: flex; align-items: flex-start; margin-bottom: 15px;">
+
+                        <div>
+                            <strong style="display: block; margin-bottom: 5px;"><i class="fas fa-check-circle" style="color: rgb(27, 180, 40); height: auto; padding-right: 5px;"></i>Sistemas de Gestión Integral (Calidad, Ambiente, Seguridad)</strong>
+                        </div>
+                    </li>
+                    <li style="display: flex; align-items: flex-start; margin-bottom: 15px;">
+
+                        <div>
+                            <strong style="display: block; margin-bottom: 5px;"><i class="fas fa-check-circle" style="color: rgb(27, 180, 40); height: auto; padding-right: 5px;"></i>Seguridad Industrial </strong>
+                        </div>
+                    </li>
+                    <li style="display: flex; align-items: flex-start; margin-bottom: 15px;">
+                        <div>
+                            <strong style="display: block; margin-bottom: 5px;"><i class="fas fa-check-circle" style="color: rgb(27, 180, 40); height: auto; padding-right: 5px;"></i>Sostenibilidad (Responsabilidad Social)</strong>
+                        </div>
+                    </li>
+
+                </ul>
+            </div> <!-- Cierre de la primera columna -->
+
+        </div>
+    </div>
+
+
+
+    <div class="row no-gutters">
+        <div class="col-12 col-md-12 d-flex flex-column justify-content-center align-items-center"
+            style="background: linear-gradient(rgba(255, 255, 255, 0.26), rgba(255, 255, 255, 0.25)),  
+                url('./assets/img/portadaS.png');
+                background-size: cover; 
+                background-attachment: fixed; 
+                background-position: center;
+                min-height: 400px;"> <!-- Establece una altura mínima de 500px -->
+
+            <!-- Contenido dentro de la columna -->
+             
+            <img src="./assets/img/logo.png" alt="logo" style="width: 40%; margin: 2%;">
+            </div>
+        </div>
+    </div>
+
+
+
+
+    <div class="col-12 d-flex flex-column justify-content-center align-items-center"
+        style="height: auto;">
+        <h1 class="titulo-contenedor text-center">Nuestro Servicio</h1>
+    </div>
+
+    <div class="container-fluid p-0">
+        <div class="row no-gutters">
+
+            <div class="col-12 col-md-6 d-flex flex-column justify-content-center align-items-center" style="background-color:rgba(231, 224, 234, 0.41); height: auto;">
+                <h1 class="tituloContenedorL text-center">1. Seguridad Industrial y Salud en el Trabajo</h1>
+                <ul style="list-style: none; padding-top: 2%; color: black;">
+                    <li style="display: flex; align-items: flex-start; margin-bottom: 15px;">
+
+                        <div>
+                            <strong style="display: block; margin-bottom: 5px;"><i class="bi bi-gear-fill" style="color: rgb(153, 15, 151); height: auto; padding-right: 5px;"></i>Diagnósticos y cumplimiento de Normativa STPS</strong>
+                        </div>
+                    </li>
+                    <li style="display: flex; align-items: flex-start; margin-bottom: 15px;">
+
+                        <div>
+                            <strong style="display: block; margin-bottom: 5px;"><i class="bi bi-shield-check" style="color: rgb(153, 15, 151); height: auto; padding-right: 5px;"></i>Diagnósticos e implementación en materia de Protección Civil.</strong>
+                        </div>
+                    </li>
+                    <li style="display: flex; align-items: flex-start; margin-bottom: 15px;">
+                        <div>
+                            <strong style="display: block; margin-bottom: 5px;"><i class="bi bi-rocket-takeoff" style="color: rgb(153, 15, 151); height: auto; padding-right: 5px;"></i>Capacitaciones Industriales STPS-con DC-3.</strong>
+                        </div>
+                    </li>
+
+                </ul>
+            </div>
+            <div class="col-12 col-md-6 d-flex flex-column justify-content-center align-items-center" style="background-color:rgb(255, 255, 255); height: auto;">
+                <img src="./assets/img/servicio1.png" alt="logo" style="width: 20%;">
+
+            </div>
+
+        </div>
+    </div>
+
+
+    <div class="container-fluid p-0">
+        <div class="row no-gutters">
+            <div class="col-12 col-md-6 d-flex flex-column justify-content-center align-items-center" style="background-color:rgb(255, 255, 255); height: auto;">
+                <img src="./assets/img/servicio2.png" alt="logo" style="width: 55%;">
+
+            </div>
+            <div class="col-12 col-md-6 d-flex flex-column justify-content-center align-items-center" style="background-color:rgba(231, 224, 234, 0.41); height: auto;">
+                <h1 class="tituloContenedorL text-center">2. Estudios Específicos de Riesgo y Seguridad</h1>
+                <ul style="list-style: none; padding-top: 2%; color: black;">
+
+                    <li style="display: flex; align-items: flex-start; margin-bottom: 15px;">
+
+                        <div>
+                            <strong style="display: block; margin-bottom: 5px;"><i class="bi bi-clipboard-check" style="color: rgb(211, 12, 12); height: auto; padding-right: 5px;"></i>Grado de Riesgo de Incendio (NOM-002-STPS-2010)</strong>
+                        </div>
+                    </li>
+                    <li style="display: flex; align-items: flex-start; margin-bottom: 15px;">
+                        <div>
+                            <strong style="display: block; margin-bottom: 5px;"><i class="bi bi-clipboard-check" style="color: rgb(211, 12, 12); height: auto; padding-right: 5px;"></i>Grado de Riesgo de Maquinaria y Equipo (NOM-004-STPS-1999)</strong>
+                        </div>
+                    </li>
+                    <li style="display: flex; align-items: flex-start; margin-bottom: 15px;">
+
+                        <div>
+                            <strong style="display: block; margin-bottom: 5px;"><i class="bi bi-clipboard-check" style="color: rgb(211, 12, 12); height: auto; padding-right: 5px;"></i>Selección, Uso y Manejo de Equipo de Protección Personal (NOM-017-STPS-2008)</strong>
+                        </div>
+                    </li>
+                    <li style="display: flex; align-items: flex-start; margin-bottom: 15px;">
+                        <div>
+                            <strong style="display: block; margin-bottom: 5px;"><i class="bi bi-clipboard-check" style="color: rgb(211, 12, 12); height: auto; padding-right: 5px;"></i>Procedimientos para almacenamiento y manejo de materiales mediante el uso de maquinaria. (NOM-006-STPS-2023)</strong>
+                        </div>
+                    </li>
+                    <li style="display: flex; align-items: flex-start; margin-bottom: 15px;">
+
+                        <div>
+                            <strong style="display: block; margin-bottom: 5px;"><i class="bi bi-clipboard-check" style="color: rgb(211, 12, 12); height: auto; padding-right: 5px;"></i>Evaluación de Normativa Aplicable, Servicios de seguridad y Salud en el trabajo (NOM-030-STPS-2009)</strong>
+                        </div>
+                    </li>
+
+                    <li style="display: flex; align-items: flex-start; margin-bottom: 15px;">
+
+                        <div>
+                            <strong style="display: block; margin-bottom: 5px;"><i class="bi bi-clipboard-check" style="color: rgb(211, 12, 12); height: auto; padding-right: 5px;"></i>Evaluación de Riesgos Ergonómicos (NOM-036-1-STPS-2018)</strong>
+                        </div>
+                    </li>
+                    <li style="display: flex; align-items: flex-start; margin-bottom: 15px;">
+                        <div>
+                            <strong style="display: block; margin-bottom: 5px;"><i class="bi bi-clipboard-check" style="color: rgb(211, 12, 12); height: auto; padding-right: 5px;"></i>Estudio sobre Factores de Riesgo Psicosocial (NOM-035-STPS-2018)</strong>
+                        </div>
+                    </li>
+                    <li style="display: flex; align-items: flex-start; margin-bottom: 15px;">
+
+                        <div>
+                            <strong style="display: block; margin-bottom: 5px;"><i class="bi bi-clipboard-check" style="color: rgb(211, 12, 12); height: auto; padding-right: 5px;"></i>Evaluación y Conformidad para Cumplimiento de Factores de Riesgo Psicosocial (Certificación)</strong>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+
+        </div>
+    </div>
+
+
+
+    <div class="container-fluid p-0">
+        <div class="row no-gutters">
+
+            <div class="col-12 col-md-6 d-flex flex-column justify-content-center align-items-center" style="background-color:rgba(231, 224, 234, 0.41); height: auto;">
+                <h1 class="tituloContenedorL text-center">3. Implementación de Sistemas de Gestión</h1>
+                <ul style="list-style: none; padding-top: 2%; color: black;">
+
+                    <li style="display: flex; align-items: flex-start; margin-bottom: 15px;">
+
+                        <div>
+                            <strong style="display: block; margin-bottom: 5px;"><i class="bi bi-arrow-right-circle" style="color: rgb(22, 153, 219); height: auto; padding-right: 5px;"></i>ISO 45001:2018 Gestión de Seguridad y Salud en el Trabajo.</strong>
+                        </div>
+                    </li>
+                    <li style="display: flex; align-items: flex-start; margin-bottom: 15px;">
+                        <div>
+                            <strong style="display: block; margin-bottom: 5px;"><i class="bi bi-arrow-right-circle" style="color: rgb(22, 153, 219); height: auto; padding-right: 5px;"></i>ISO 14001:2015 - Sistema de Gestión Ambiental.</strong>
+                        </div>
+                    </li>
+                    <li style="display: flex; align-items: flex-start; margin-bottom: 15px;">
+
+                        <div>
+                            <strong style="display: block; margin-bottom: 5px;"><i class="bi bi-arrow-right-circle" style="color: rgb(22, 153, 219); height: auto; padding-right: 5px;"></i>ISO 9001:2015- Sistemas de gestión de la calidad.</strong>
+                        </div>
+                    </li>
+                    <li style="display: flex; align-items: flex-start; margin-bottom: 15px;">
+                        <div>
+                            <strong style="display: block; margin-bottom: 5px;"><i class="bi bi-arrow-right-circle" style="color: rgb(22, 153, 219); height: auto; padding-right: 5px;"></i>RS10 IQNET Sistema de Gestión en Responsabilidad Social.</strong>
+                        </div>
+                    </li>
+                    <li style="display: flex; align-items: flex-start; margin-bottom: 15px;">
+
+                        <div>
+                            <strong style="display: block; margin-bottom: 5px;"><i class="bi bi-arrow-right-circle" style="color: rgb(22, 153, 219); height: auto; padding-right: 5px;"></i>BASC: "Business Alliance for Secure Commerce".</strong>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-12 col-md-6 d-flex flex-column justify-content-center align-items-center" style="background-color:rgb(255, 255, 255); height: auto;">
+                <img src="./assets/img/servicio3.png" alt="logo" style="width: 70%;">
+
+            </div>
+
+        </div>
+    </div>
+
+</main>
+
+
+<!-- EQUIPO FIN -->
+
+
+<?php include("./includes/footer.php"); ?>
